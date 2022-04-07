da082ffc7cf6b1233e6fd9c099d9d9591cdc1743
Jefferson Oliveira <jeffebido@hotmail.com>
2022-04-07 15:14:01 -0300

    Todos os requisitos implementados

--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
 let jogadas = 0;
+let par = 0;
 let baralho = Array("bobrossparrot", "explodyparrot", "fiestaparrot", "metalparrot", "revertitparrot", "tripletsparrot", "unicornparrot");
+let wait = false;
+let numCartas = 0;
 
 function initGame(){
 
-  let numCartas = window.prompt("Com quantas cartas quer jogar? 🦜");
+  numCartas = window.prompt("Com quantas cartas quer jogar? 🦜");
 
   numCartas = parseInt(numCartas);
 
   if(numCartas < 4 || numCartas > 14 || numCartas%2 !== 0){
     location.reload();
   }else{
-    distribuiCartas ( embaralha(numCartas) );
+    distribuiCartas ( embaralha() );
   }
   
 }
 
-function embaralha(numCartas){
+function embaralha(){
 
   let cartas = Array();
 
@@ -40,7 +43,7 @@ function distribuiCartas(cartas){
 
     html += `
             <div class="col">
-              <div class="card" onclick="flip(this)">
+              <div class="card" onclick="flip(this)" card="${cartas[i]}">
                   <div class="card-front"></div>
                   <div class="card-back">
                       <img src="img/${cartas[i]}.gif"/>
@@ -63,12 +66,85 @@ function distribuiCartas(cartas){
 
 function flip(card){
   
-  card.classList.toggle("flipped");
+  if(!card.classList.contains("par-formado") && !wait){
+    
+    let anterior = document.querySelector(".anterior");
+    let paresFormados = document.getElementsByClassName("par-formado");
+    let viradas = document.getElementsByClassName("flipped");
+
+
+    if( !card.classList.contains("flipped") ){
+
+      card.classList.add("flipped");
+      
+    }
+    
+  
+
+    if(anterior !== null){
+
+      
+
+      if(anterior.getAttribute("card") === card.getAttribute("card") ){
+
+        anterior.classList.add("par-formado");
+        card.classList.add("par-formado");
+      }else{
+        wait = true;
+        setTimeout(function(){
+          anterior.classList.remove("flipped");
+          card.classList.remove("flipped");
+          wait = false;
+        },1000);
+
+      }
+
+      anterior.classList.remove("anterior");
+    }else{
+      card.classList.add("anterior");
+    }
+
+    jogadas++;
+
+    if(numCartas == paresFormados.length){
+      setTimeout(function(){
+        encerraJogo();
+      },1000);
+    }
+     
+  }
+}
+
+function encerraJogo(){
+
+  alert("Você ganhou em "+ jogadas +" jogadas!");
+
+  while( !jogarNovamente() ){
+    
+  }
+  
+  
+}
+
+function jogarNovamente(){
+
+  let jogarNovamente = window.prompt("Gostaria de reiniciar a partida?");
+
+  if( jogarNovamente == "sim"){
+    location.reload();
+    return true;
+  }else if(jogarNovamente == "não"){
+    alert("Obrigado por jogar!");
+    return true;
+  }else{
+    return false;
+  }
 }
 
 function comparador() { 
 	return Math.random() - 0.5; 
 }
 
+ 
 
 initGame();
